006701c599eedf8a5390927a6028f3ae77ea5210
Ted <bruno.cocielli@hotmail.com>
2026-01-17 10:57:38 -0300

    added portfolio folder

---
+++ portfolio/js/main.js
@@ -0,0 +1,25 @@
+const links = document.querySelectorAll(".hero-nav a")
+const pages = document.querySelectorAll(".page")
+
+links.forEach(link => {
+  link.addEventListener("click", event => {
+    event.preventDefault()
+
+    const targetId = link.dataset.section
+
+    pages.forEach(page => {
+      page.classList.remove("active")
+    })
+
+    const targetPage = document.getElementById(targetId)
+    targetPage.classList.add("active")
+
+    links.forEach(l => {
+      l.classList.remove("active")
+    })
+
+    link.classList.add("active")
+  })
+})
+
+document.querySelector('.hero-nav a[data-section="projects"]').classList.add("active")
